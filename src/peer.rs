0aa67cf53260323b7288450b5c29268665853555
MrZloHex <zlo.alex.it@gmail.com>
2021-08-05 15:40:44 +0300

    SPEAK

--- src/peer.rs
+++ src/peer.rs
@@ -1,7 +1,12 @@
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
+use std::io::{Read, Write};
+use std::str::from_utf8;
+
 
 pub struct Peer {
-    period: u16,
+    period: u64,
     me: TcpListener,
     peers: Option<Vec<TcpStream>>
 }
@@ -11,21 +16,21 @@ impl Peer{
         let address = format!("127.0.0.1:{}", port);
         println!("{}", address);
 
-        let mut peer = if let Some(an_peer) = init_peer {
+        let peer = if let Some(an_peer) = init_peer {
             let stream = match TcpStream::connect(an_peer) {
-                Ok(mut stream) => stream,
+                Ok(stream) => stream,
                 Err(e) => panic!("CAN'T CONNECT {}", e)
             };
 
 
             Peer {
-                period: per.parse::<u16>().unwrap(),
+                period: per.parse::<u64>().unwrap(),
                 me: TcpListener::bind(address).unwrap(),
                 peers: Some(vec![stream])
             }
         } else {
             Peer { 
-                period: per.parse::<u16>().unwrap(),
+                period: per.parse::<u64>().unwrap(),
                 me: TcpListener::bind(address).unwrap(),
                 peers: None
             }
@@ -35,6 +40,38 @@ impl Peer{
     }
 
     pub fn start_communication(&mut self) {
+        thread::spawn(|| {
+            self.speak();
+            thread::sleep(Duration::from_secs(self.period));
+        });
 
+
+    }
+
+
+    fn speak(&mut self) {
+        if let Some(peers) = self.peers {
+            for peer in peers {
+                let msg = b"Hello!";
+
+                peer.write(msg).unwrap();
+                println!("Sent Hello, awaiting reply...");
+
+                let mut data = [0 as u8; 6]; // using 6 byte buffer
+                match peer.read_exact(&mut data) {
+                    Ok(_) => {
+                        if &data == msg {
+                            println!("Reply is ok!");
+                        } else {
+                            let text = from_utf8(&data).unwrap();
+                            println!("Unexpected reply: {}", text);
+                        }
+                    },
+                    Err(e) => {
+                        println!("Failed to receive data: {}", e);
+                    }
+                }
+            }
+        }
     }
 }
