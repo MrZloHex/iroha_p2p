ff1e44dd2ea812c1de6215f1a0cbdeddbce40739
MrZloHex <zlo.alex.it@gmail.com>
2021-08-05 14:03:18 +0300

    CLI

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use clap::{App, load_yaml};
 fn main() {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from_yaml(yaml).get_matches();
-
+    if let Some(period) = matches.value_of("period") {
+        println!("{}", period);
+    }
 }
 
