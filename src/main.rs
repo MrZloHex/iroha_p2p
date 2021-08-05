dcdbeed5b610bcf320a422531bbf6aec6352093c
MrZloHex <zlo.alex.it@gmail.com>
2021-08-05 13:56:54 +0300

    FUCK

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern crate clap;
+use clap::{App, load_yaml};
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from_yaml(yaml).get_matches();
+
 }
+
