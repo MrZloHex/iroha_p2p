7de063a18806f99f1b4770ae109bc4906148e809
MrZloHex <zlo.alex.it@gmail.com>
2021-08-11 10:51:30 +0300

    p2p_from_std_net

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,35 @@
-extern crate clap;
-use clap::{load_yaml, App};
+//extern crate clap;
+//use clap::{load_yaml, App};
+//
+//mod fw;
+//mod peer;
+//use peer::Peer;
 
-mod fw;
-mod peer;
-use peer::Peer;
+extern crate p2p_from_std_net;
+use p2p_from_std_net::Peer;
+
 
 fn main() {
     // Loading configuration of cli arguments
-    let yaml = load_yaml!("cli.yaml");
-    // Compliance check
-    let matches = App::from_yaml(yaml).get_matches();
+    //let yaml = load_yaml!("cli.yaml");
+    //// Compliance check
+    //let matches = App::from_yaml(yaml).get_matches();
 
-    // Assigment of arguments
-    let period = if let Some(period) = matches.value_of("period") {
-        period
-    } else {
-        "5"
-    };
-    let port = if let Some(port) = matches.value_of("port") {
-        port
-    } else {
-        "8080"
-    };
-    let filename: Option<&str> = matches.value_of("list");
+    //// Assigment of arguments
+    //let period = if let Some(period) = matches.value_of("period") {
+    //    period
+    //} else {
+    //    "5"
+    //};
+    //let port = if let Some(port) = matches.value_of("port") {
+    //    port
+    //} else {
+    //    "8080"
+    //};
+    //let filename: Option<&str> = matches.value_of("list");
 
-    // Starting Peer
-    Peer::start(period, port, filename);
+    //// Starting Peer
+    //Peer::start(period, port, filename);
+
+
 }
